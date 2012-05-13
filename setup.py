28366b7634126226c1e0f0f10604cbf3e3223bc0
Brian Sutherland <brian@vanguardistas.net>
2012-05-13 21:45:03 +0200

    Add change log, tox.ini and refine metadata.

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 _here = os.path.dirname(__file__)
 README = open(os.path.join(_here, 'van', 'pg', 'README.txt'), 'r').read()
+CHANGES = open(os.path.join(_here, 'CHANGES.txt'), 'r').read()
 
 setup(name="van.pg",
-      version="1.4",
+      version="2.0",
       description="Tools to programmatically manage PostgreSQL clusters as Python test fixtures.",
       packages=find_packages(),
-      long_description=README,
+      long_description=README + '\n' + CHANGES,
       license='BSD',
       author="Brian Sutherland",
       author_email='brian@vanguardistas.net',
@@ -17,15 +18,17 @@ setup(name="van.pg",
                         'testresources',
                         ],
       classifiers=[
-          "Development Status :: 4 - Beta",
+          "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
           "Operating System :: OS Independent",
           "License :: OSI Approved :: BSD License",
           "Topic :: Database",
           "Topic :: Software Development :: Testing",
-          "Programming Language :: Python :: 2.5",
+          "Programming Language :: Python :: 2",
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
           ],
       tests_require=[
           'psycopg2',
